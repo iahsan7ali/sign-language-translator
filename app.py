81f2124761d2b3cafd7f108bf0c213e2e26a02c1
iahsan7ali <ahsa16725@gmail.com>
2026-01-30 00:06:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 import cv2
 import mediapipe as mp
-mp_holistic = mp.solutions.holistic
-mp_drawing = mp.solutions.drawing_utils
 import numpy as np
 import torch
 import torch.nn as nn
 import pickle
 
-
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
 
 class SignLanguageFinal(nn.Module):
     def __init__(self, num_classes):
@@ -59,9 +58,6 @@ model, actions, device = load_resources()
 class SignTransformer(VideoTransformerBase):
     def __init__(self):
         self.mp_holistic = mp_holistic.Holistic(
-         static_image_mode=False,
-         model_complexity=1,
-         smooth_landmarks=True,
          min_detection_confidence=0.5,
          min_tracking_confidence=0.5
           )
